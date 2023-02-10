03044bf9ecea82aa8e520c5b0a97baa1ccd8aa45
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2023-02-10 21:10:38 +0100

    Exp works with sampling, PDF, CDF and ICDF along with uniform, and testing framework is set up

--- Random.c
+++ Random.c
@@ -3,43 +3,188 @@
 #include "Random.h"
 #include <time.h>
 
+void PrintIntArray(char *Name, uint64_t *Array, size_t Length);
+
+void PrintFloatArray(char *Name, double *Array, size_t Length);
+
+double *HistInt(uint64_t Min, uint64_t Max, uint32_t Bins, uint64_t *Array, size_t Size);
+
+double *HistFloat(double Min, double Max, uint32_t Bins, double *Array, size_t Size);
+
+double *linspace(double Min, double Max, uint32_t Length);
+
+uint64_t *arange(uint64_t Min, uint64_t Step, uint32_t Length);
+
 // Test Random.h
 int main(int argc, char **argv)
 {
+    size_t Size = 100000;
+    double *Hist = NULL;
+    uint64_t *IntArray = NULL;
+    double *FloatArray = NULL;
+    uint32_t Bins = 10;
+    double *xFloat = NULL;
+    uint64_t *xInt = NULL;
+
     // Generate seed
     uint64_t Seed = RNG_GenSeed();
-    printf("Seed: %u\n", Seed);
+    printf("Seed: %lu\n", Seed);
 
-    // Generate some random integers
-    printf("Random ints: %X, %X, %X\n", RNG_Int(&Seed), RNG_Int(&Seed), RNG_Int(NULL));
+    // Generate uniform ints
+    printf("Uniform ints: %lu, %lu, %lu\n", RNG_Int(&Seed, 0, Bins - 1), RNG_Int(&Seed, 0, Bins - 1), RNG_Int(NULL, 0, Bins - 1));
 
-    // Generate some random floats
-    printf("Random floats: %.3g, %.3g, %.3g\n", RNG_Float(&Seed), RNG_Float(&Seed), RNG_Float(NULL));
-
-    size_t Size = 10;
-
-    // Generate int list
-    uint64_t *IntArray = RNG_Int_Array(&Seed, Size);
-
-    printf("Random int array: [");
-
-    for (uint64_t *List = IntArray, *EndList = IntArray + Size - 1; List < EndList; ++List)
-        printf("%u, ", *List % 100);
-
-    printf("%u]\n", IntArray[Size - 1] % 100);
-
-    // Generate int list
-    double *FloatArray = RNG_Float_Array(&Seed, Size);
-
-    printf("Random float array: [");
-
-    for (double *List = FloatArray, *EndList = FloatArray + Size - 1; List < EndList; ++List)
-        printf("%.3g, ", *List);
-
-    printf("%.3g]\n", FloatArray[Size - 1]);
+    IntArray = RNG_Int_Array(&Seed, 0, Bins - 1, Size);
+    Hist = HistInt(0, 9, Bins, IntArray, Size);
+    PrintFloatArray("Uniform int distribution", Hist, Bins);
 
     free(IntArray);
+    free(Hist);
+
+    // Generate uniform floats
+    printf("Uniform floats: %.3g, %.3g, %.3g\n", RNG_Float(&Seed, 0, 1), RNG_Float(&Seed, 0, 1), RNG_Float(NULL, 0, 1));
+
+    FloatArray = RNG_Float_Array(&Seed, 0, 1, Size);
+    Hist = HistFloat(0, 1, Bins, FloatArray, Size);
+    PrintFloatArray("Uniform float distribution", Hist, Bins);
+
+    free(FloatArray);
+    free(Hist);
+
+    // Generate exp
+    printf("Exp: %.3g, %.3g, %.3g\n", RNG_Exp(&Seed, 1), RNG_Exp(&Seed, 1), RNG_Exp(NULL, 1));
+
+    FloatArray = RNG_Exp_Array(&Seed, 1, Size);
+    Hist = HistFloat(0, 5, Bins, FloatArray, Size);
+    PrintFloatArray("Exp distribution", Hist, Bins);
+
+    free(FloatArray);
+    free(Hist);
+
+    printf("Exp PDF: %.3g, %.3g, %.3g\n", RNG_ExpPDF(0, 1), RNG_ExpPDF(1, 1), RNG_ExpPDF(2, 1));
+
+    xFloat = linspace(0, 5, Bins);
+    FloatArray = RNG_ExpPDF_Array(xFloat, 1, Size);
+    PrintFloatArray("Exp PDF distribution", FloatArray, Bins);
+
+    free(xFloat);
+    free(FloatArray);
+
+    printf("Exp CDF: %.3g, %.3g, %.3g\n", RNG_ExpCDF(0, 1), RNG_ExpCDF(1, 1), RNG_ExpCDF(2, 1));
+
+    xFloat = linspace(0, 5, Bins);
+    FloatArray = RNG_ExpCDF_Array(xFloat, 1, Size);
+    PrintFloatArray("Exp CDF distribution", FloatArray, Bins);
+
+    free(xFloat);
+    free(FloatArray);
+
+    printf("Exp ICDF: %.3g, %.3g, %.3g\n", RNG_ExpICDF(0, 1), RNG_ExpICDF(0.4, 1), RNG_ExpICDF(0.8, 1));
+
+    xFloat = linspace(0, 1, Bins);
+    FloatArray = RNG_ExpICDF_Array(xFloat, 1, Size);
+    PrintFloatArray("Exp ICDF distribution", FloatArray, Bins);
+
+    free(xFloat);
     free(FloatArray);
 
     return 0;
+}
+
+void PrintIntArray(char *Name, uint64_t *Array, size_t Length)
+{
+    printf("%s: [", Name);
+
+    for (uint64_t *List = Array, *EndList = Array + Length - 1; List < EndList; ++List)
+        printf("%lu, ", *List);
+
+    printf("%lu]\n", Array[Length - 1]);
+}
+
+void PrintFloatArray(char *Name, double *Array, size_t Length)
+{
+    printf("%s: [", Name);
+
+    for (double *List = Array, *EndList = Array + Length - 1; List < EndList; ++List)
+        printf("%.3g, ", *List);
+
+    printf("%.3g]\n", Array[Length - 1]);
+}
+
+double *HistInt(uint64_t Min, uint64_t Max, uint32_t Bins, uint64_t *Array, size_t Size)
+{
+    // Get bins
+    double *BinCounts = (double *)malloc(sizeof(double) * Bins);
+
+    for (double *List = BinCounts, *EndList = BinCounts + Bins; List < EndList; ++List)
+        *List = 0;
+
+    // Fill them up
+    for (uint64_t *Numbers = Array, *EndNumbers = Array + Size; Numbers < EndNumbers; ++Numbers) 
+    {
+        int64_t Index = (*Numbers - Min) * Bins / (1 + Max - Min);
+
+        if (Index >= 0 && Index < Bins)
+            BinCounts[Index] += 1;
+    }
+
+    for (double *List = BinCounts, *EndList = BinCounts + Bins; List < EndList; ++List)
+        *List /= Size * (1 + Max - Min) / Bins;
+
+    return BinCounts;
+}
+
+double *HistFloat(double Min, double Max, uint32_t Bins, double *Array, size_t Size)
+{
+    // Get bins
+    double *BinCounts = (double *)malloc(sizeof(double) * Bins);
+
+    for (double *List = BinCounts, *EndList = BinCounts + Bins; List < EndList; ++List)
+        *List = 0;
+
+    // Fill them up
+    for (double *Numbers = Array, *EndNumbers = Array + Size; Numbers < EndNumbers; ++Numbers)
+    {
+        int64_t Index = (uint64_t) ((*Numbers - Min) * Bins / (Max - Min));
+
+        if (Index >= 0 && Index < Bins)
+            BinCounts[Index] += 1;
+    }
+
+    for (double *List = BinCounts, *EndList = BinCounts + Bins; List < EndList; ++List)
+        *List /= Size * (Max - Min) / Bins;
+
+    return BinCounts;
+}
+
+double *linspace(double Min, double Max, uint32_t Length)
+{
+    double *Values = (double *)malloc(sizeof(double) * Length);
+
+    double Step = (Max - Min) / (double) Length;
+    Min += Step / 2;
+    Max -= Step / 2;
+    double Prev = Min - Step;
+
+    for (double *List = Values, *EndList = Values + Length; List < EndList; ++List)
+    {
+        Prev += Step;
+        *List = Prev;
+    }
+
+    return Values;
+}
+
+uint64_t *arange(uint64_t Min, uint64_t Step, uint32_t Length)
+{
+    uint64_t *Values = (uint64_t *)malloc(sizeof(uint64_t) * Length);
+
+    uint64_t Prev = Min - Step;
+
+    for (uint64_t *List = Values, *EndList = Values + Length; List < EndList; ++List)
+    {
+        Prev += Step;
+        *List = Prev;
+    }
+
+    return Values;
 }
--- Random.h
+++ Random.h
@@ -5,17 +5,147 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define ERR_PREFIX RNG
 #include <Error.h>
 
-enum _RNG_ErrorID {
+enum _RNG_ErrorID
+{
     _RNG_ERRORID_GENERAL_NONE = 0x700000000,
     _RNG_ERRORID_INTARRAY_MALLOC = 0x700010200,
-    _RNG_ERRORID_FLOATARRAY_MALLOC = 0x700020200
+    _RNG_ERRORID_FLOATARRAY_MALLOC = 0x700020200,
+    _RNG_ERRORID_EXPARRAY_MALLOC = 0x700030200,
+    _RNG_ERRORID_EXPPDFARRAY_MALLOC = 0x700040200,
+    _RNG_ERRORID_EXPCDFARRAY_MALLOC = 0x700050200,
+    _RNG_ERRORID_EXPICDFARRAY_MALLOC = 0x700060200
 };
 
 #define _RNG_ERRORMES_MALLOC "Unable to allocate memory (Size: %lu)"
+
+// Get a uniform random int
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+uint64_t RNG_Int(uint64_t *Seed, uint64_t Min, uint64_t Max);
+
+// Get an array of uniform random int
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+// Size: The size of the array
+uint64_t *RNG_Int_Array(uint64_t *Seed, uint64_t Min, uint64_t Max, size_t Size);
+
+// Fill an array with uniform random int
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+// Array The array to fill
+// Size: The size of the array
+void RNG_Int_ArrayM(uint64_t *Seed, uint64_t Min, uint64_t Max, uint64_t *Array, size_t Size);
+
+// Get a uniform random float
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+double RNG_Float(uint64_t *Seed, double Min, double Max);
+
+// Get an array of uniform random float
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+// Size: The size of the array
+double *RNG_Float_Array(uint64_t *Seed, double Min, double Max, size_t Size);
+
+// Fill an array with uniform random float
+// Seed: The seed to use and update, NULL to use global seed
+// Min: The minimum value
+// Max: The maximum value
+// Array The array to fill
+// Size: The size of the array
+void RNG_Float_ArrayM(uint64_t *Seed, double Min, double Max, double *Array, size_t Size);
+
+// Get a random number from an exponential distibution (1 / l * exp(-x / l))
+// Seed: The seed to use and update, NULL to use global seed
+// l: The length scale of the distribution
+double RNG_Exp(uint64_t *Seed, double l);
+
+// Get an array of random numbers from an exponential distribution (1 / l * exp(-x / l))
+// Seed: The seed to use and update, NULL to use global seed
+// l: The length scale of the distribution
+// Size: The size of the array
+double *RNG_Exp_Array(uint64_t *Seed, double l, size_t Size);
+
+// Fill an array with random numbers from an exponential distribution (1 / l * exp(-x / l))
+// Seed: The seed to use and update, NULL to use global seed
+// l: The length scale of the distribution
+// Array The array to fill
+// Size: The size of the array
+void RNG_Exp_ArrayM(uint64_t *Seed, double l, double *Array, size_t Size);
+
+// Get the PDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-value to get the y-value for
+// l: The length scale of the distribution
+double RNG_ExpPDF(double x, double l);
+
+// Get an array of the PDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Size: The size of the array
+double *RNG_ExpPDF_Array(double *x, double l, size_t Size);
+
+// Fill an array of the PDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Array The array to fill
+// Size: The size of the array
+void *RNG_ExpPDF_ArrayM(double *x, double l, double *Array, size_t Size);
+
+// Get the CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-value to get the y-value for
+// l: The length scale of the distribution
+double RNG_ExpCDF(double x, double l);
+
+// Get an array of the CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Size: The size of the array
+double *RNG_ExpCDF_Array(double *x, double l, size_t Size);
+
+// Fill an array of the CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Array The array to fill
+// Size: The size of the array
+void *RNG_ExpCDF_ArrayM(double *x, double l, double *Array, size_t Size);
+
+// Get the inverse CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-value to get the y-value for
+// l: The length scale of the distribution
+double RNG_ExpICDF(double y, double l);
+
+// Get an array of the inverse CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Size: The size of the array
+double *RNG_ExpICDF_Array(double *y, double l, size_t Size);
+
+// Fill an array of the inverse CDF for an exponential distribution (1 / l * exp(-x / l))
+// x: The x-values to get the y-value for
+// l: The length scale of the distribution
+// Array The array to fill
+// Size: The size of the array
+void *RNG_ExpICDF_ArrayM(double *y, double l, double *Array, size_t Size);
+
+// DoubleExp
+
+// Normal
+
+// Poisson
+
+// Binomial
+
+// Monte carlo
 
 // Constants
 #define RNG_MAX 18446744073709551616.
@@ -35,30 +165,18 @@ uint64_t _RNG_GlobalSeed = 0;
 // Generates a seed, returns it as a uint32
 #define RNG_GenSeed() ((uint64_t) time(NULL) ^ (uint64_t) clock())
 
-
-// Get a random uint64_t and update the seed to be that number
-// Seed (uint64_t): The seed to use and update, NULL to use global seed
-uint64_t RNG_Int(uint64_t *Seed)
+uint64_t RNG_Int(uint64_t *Seed, uint64_t Min, uint64_t Max)
 {
     extern uint64_t _RNG_GlobalSeed;
 
     if (Seed == NULL)
         Seed = &_RNG_GlobalSeed;
 
-    return RNG_FastInt(*Seed);
+    return Min + (RNG_FastInt(*Seed) % (1 + Max - Min));
 }
 
-// Get an array of random uint64_t and updates the seed
-// Seed (uint64_t): The seed to use and update, NULL to use global seed
-// Size (size_t): The size of the array
-uint64_t *RNG_Int_Array(uint64_t *Seed, size_t Size)
+uint64_t *RNG_Int_Array(uint64_t *Seed, uint64_t Min, uint64_t Max, size_t Size)
 {
-    // Get the global seed
-    extern uint64_t _RNG_GlobalSeed;
-
-    if (Seed == NULL)
-        Seed = &_RNG_GlobalSeed;
-
     // Get memory
     uint64_t *Array = (uint64_t *)malloc(sizeof(uint64_t) * Size);
 
@@ -68,29 +186,12 @@ uint64_t *RNG_Int_Array(uint64_t *Seed, size_t Size)
         return NULL;
     }
 
-    // Fill memory
-    for (uint64_t *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List)
-        *List = RNG_FastInt(*Seed);
+    RNG_Int_ArrayM(Seed, Min, Max, Array, Size);
 
     return Array;
 }
 
-// Get a random double between 0 inclusive and 1 exclusive and update seed
-// Seed (uint64_t): The seed to use and update, NULL to use global seed
-double RNG_Float(uint64_t *Seed)
-{
-    extern uint64_t _RNG_GlobalSeed;
-
-    if (Seed == NULL)
-        Seed = &_RNG_GlobalSeed;
-
-    return RNG_FastFloat(*Seed);
-}
-
-// Get an array of random double between 0 inclusive and 1 exclusive and update seed
-// Seed (uint64_t): The seed to use and update, NULL to use global seed
-// Size (size_t): The size of the array
-double *RNG_Float_Array(uint64_t *Seed, size_t Size)
+void RNG_Int_ArrayM(uint64_t *Seed, uint64_t Min, uint64_t Max, uint64_t *Array, size_t Size)
 {
     // Get the global seed
     extern uint64_t _RNG_GlobalSeed;
@@ -98,6 +199,23 @@ double *RNG_Float_Array(uint64_t *Seed, size_t Size)
     if (Seed == NULL)
         Seed = &_RNG_GlobalSeed;
 
+    // Fill memory
+    for (uint64_t *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List)
+        *List = Min + (RNG_FastInt(*Seed) % (1 + Max - Min));
+}
+
+double RNG_Float(uint64_t *Seed, double Min, double Max)
+{
+    extern uint64_t _RNG_GlobalSeed;
+
+    if (Seed == NULL)
+        Seed = &_RNG_GlobalSeed;
+
+    return Min + RNG_FastFloat(*Seed) * (Max - Min);
+}
+
+double *RNG_Float_Array(uint64_t *Seed, double Min, double Max, size_t Size)
+{
     // Get memory
     double *Array = (double *)malloc(sizeof(double) * Size);
 
@@ -107,11 +225,176 @@ double *RNG_Float_Array(uint64_t *Seed, size_t Size)
         return NULL;
     }
 
-    // Fill memory
-    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List)
-        *List = RNG_FastFloat(*Seed);
+    // Get numbers
+    RNG_Float_ArrayM(Seed, Min, Max, Array, Size);
 
     return Array;
+}
+
+void RNG_Float_ArrayM(uint64_t *Seed, double Min, double Max, double *Array, size_t Size)
+{
+    // Get the global seed
+    extern uint64_t _RNG_GlobalSeed;
+
+    if (Seed == NULL)
+        Seed = &_RNG_GlobalSeed;
+
+    // Fill memory
+    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List)
+        *List = Min + RNG_FastFloat(*Seed) * (Max - Min);
+}
+
+double RNG_Exp(uint64_t *Seed, double l)
+{
+    // Get the global seed
+    extern uint64_t _RNG_GlobalSeed;
+
+    if (Seed == NULL)
+        Seed = &_RNG_GlobalSeed;
+
+    // Get the number from the uniform distribution
+    double Uniform = RNG_FastFloat(*Seed);
+
+    // Get from current distribution
+    return -l * log(1 - Uniform);
+}
+
+double *RNG_Exp_Array(uint64_t *Seed, double l, size_t Size)
+{
+    // Get memory
+    double *Array = (double *)malloc(sizeof(double) * Size);
+
+    if (Array == NULL)
+    {
+        _RNG_SetError(_RNG_ERRORID_EXPARRAY_MALLOC, _RNG_ERRORMES_MALLOC, sizeof(double) * Size);
+        return NULL;
+    }
+
+    // Get numbers
+    RNG_Exp_ArrayM(Seed, l, Array, Size);
+
+    return Array;
+}
+
+void RNG_Exp_ArrayM(uint64_t *Seed, double l, double *Array, size_t Size)
+{
+    // Get the global seed
+    extern uint64_t _RNG_GlobalSeed;
+
+    if (Seed == NULL)
+        Seed = &_RNG_GlobalSeed;
+
+    // Fill memory
+    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List)
+    {
+        // Get uniform number
+        double Uniform = RNG_FastFloat(*Seed);
+
+        // Get from distribution
+        *List = -l * log(1 - Uniform);
+    }
+}
+
+double RNG_ExpPDF(double x, double l)
+{
+    // Calculate constants
+    double c = 1 / l;
+
+    // Calculate the PDF
+    return ((x < 0) ? (0) : (c * exp(-c * x)));
+}
+
+double *RNG_ExpPDF_Array(double *x, double l, size_t Size)
+{
+    // Get memory
+    double *Array = (double *)malloc(sizeof(double) * Size);
+
+    if (Array == NULL)
+    {
+        _RNG_SetError(_RNG_ERRORID_EXPPDFARRAY_MALLOC, _RNG_ERRORMES_MALLOC, sizeof(double) * Size);
+        return NULL;
+    }
+
+    // Get numbers
+    RNG_ExpPDF_ArrayM(x, l, Array, Size);
+
+    return Array;
+}
+
+void *RNG_ExpPDF_ArrayM(double *x, double l, double *Array, size_t Size)
+{
+    // Calculate constants
+    double c = 1 / l;
+
+    // Fill memory
+    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List, ++x)
+        *List = ((*x < 0) ? (0) : (c * exp(-c * *x)));
+}
+
+double RNG_ExpCDF(double x, double l)
+{
+    // Calculate constants
+    double c = 1 / l;
+
+    // Calculate the CDF
+    return ((x < 0) ? (0) : (1 - exp(-c * x)));
+}
+
+double *RNG_ExpCDF_Array(double *x, double l, size_t Size)
+{
+    // Get memory
+    double *Array = (double *)malloc(sizeof(double) * Size);
+
+    if (Array == NULL)
+    {
+        _RNG_SetError(_RNG_ERRORID_EXPCDFARRAY_MALLOC, _RNG_ERRORMES_MALLOC, sizeof(double) * Size);
+        return NULL;
+    }
+
+    // Get numbers
+    RNG_ExpCDF_ArrayM(x, l, Array, Size);
+
+    return Array;
+}
+
+void *RNG_ExpCDF_ArrayM(double *x, double l, double *Array, size_t Size)
+{
+    // Calculate constants
+    double c = 1 / l;
+
+    // Fill memory
+    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List, ++x)
+        *List = ((*x < 0) ? (0) : (1 - exp(-c * *x)));
+}
+
+double RNG_ExpICDF(double y, double l)
+{
+    // Calculate the ICDF
+    return -l * log(1 - y);
+}
+
+double *RNG_ExpICDF_Array(double *y, double l, size_t Size)
+{
+    // Get memory
+    double *Array = (double *)malloc(sizeof(double) * Size);
+
+    if (Array == NULL)
+    {
+        _RNG_SetError(_RNG_ERRORID_EXPICDFARRAY_MALLOC, _RNG_ERRORMES_MALLOC, sizeof(double) * Size);
+        return NULL;
+    }
+
+    // Get numbers
+    RNG_ExpICDF_ArrayM(y, l, Array, Size);
+
+    return Array;
+}
+
+void *RNG_ExpICDF_ArrayM(double *y, double l, double *Array, size_t Size)
+{
+    // Fill memory
+    for (double *List = Array, *ListEnd = Array + Size; List < ListEnd; ++List, ++y)
+        *List = -l * log(1 - *y);
 }
 
 #endif // RANDOM2_H_INCLUDED
